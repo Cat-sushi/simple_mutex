b5af7040ac957590515af0e2209349b88add6795
Katsushi Yako <katsushi.yako@gmail.com>
2022-11-21 21:31:51 +0900

    Update doc/ license comments

--- lib/simple_mutex.dart
+++ lib/simple_mutex.dart
@@ -1,5 +1,4 @@
-// Copyright (c) 2022, Yako.
-// Please see the AUTHORS filefor details. All rights reserved.
+// Copyright (c) 2022, Yako. All rights reserved.
 // Use of this source code is governed by a BSD-style license
 // that can be found in the LICENSE file.
 
